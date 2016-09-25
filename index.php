06f7d9ffc3feaba4e24cc8b5298b61aa79a7afe7
MaartenGDev <maarten.grootoonk@gmail.com>
2016-09-25 18:45:45 +0200

    Added project design

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
+use MaartenGDev\Client;
+use MaartenGDev\DescriptionParser;
 require_once 'vendor/autoload.php';
+
 $guzzle = new GuzzleHttp\Client();
 $source = $_SERVER['DOCUMENT_ROOT'] . '/cache/Attractions.json';
-$parser = new \MaartenGDev\DescriptionParser($source);
 
-$client = new \MaartenGDev\Client($guzzle,$parser);
+$parser = new DescriptionParser($source);
+
+$client = new Client($guzzle,$parser);
 
 
 echo $client->searchAndExclude('leuk','huwelijksvoltrekking');
